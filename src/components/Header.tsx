86fad08efebbc8477b610b0107e0bcb0cf97c5b4
alexadrien <alexadriena@sipios.com>
2021-12-29 21:47:39 +0100

    Add subtitle

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,9 +8,14 @@ const Header: FC = () => {
       <Box
         sx={(theme) => ({
           marginTop: theme.spacing(2),
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
         })}
       >
         <Typography variant={"h1"}>PicAAArd</Typography>
+        <Typography variant={"subtitle1"}>Powered by A³</Typography>
       </Box>
     </LineBox>
   );
